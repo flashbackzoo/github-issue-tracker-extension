8b1c89ee5c2f88eb33f9b7513d4b196cd51d8dd8
David Craig <david@davidcraig.co.nz>
2016-04-30 15:06:50 +1200

    Add watch tasks to build

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ const buffer = require('vinyl-buffer');
 const cleancss = require('gulp-clean-css');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
+const notify = require('gulp-notify');
 const packageJson = require('./package.json');
 const sass = require('gulp-sass');
 const semver = require('semver');
 const source = require('vinyl-source-stream');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
-const watch = require('gulp-watch');
+const watchify = require('watchify');
 
 // Make sure the version of Node being used is valid.
 if (!semver.satisfies(process.versions.node, packageJson.engines.node)) {
@@ -27,22 +28,30 @@ process.env.NODE_ENV = typeof process.env.npm_config_development !== 'undefined'
   ? 'development'
   : 'production';
 
-gulp.task('build', ['browserify', 'copy', 'css']);
+gulp.task('build', ['browserify', 'copy', 'css', 'css:watch']);
 
-gulp.task('browserify', () => {
-  const b = browserify({
+gulp.task('browserify', function bundlePopup() {
+  const options = {
     entries: './src/Popup/Popup.js',
     debug: true,
     transform: [babelify],
-  });
+  };
+
+  if (process.env.NODE_ENV === 'development') {
+    options.plugin = [watchify];
+  }
+
+  const b = browserify(options)
+    .on('update', bundlePopup)
+    .on('log', (msg) => { gutil.log('Finished', `./dist/Popup.js ${msg}`); });
 
   return b.bundle()
+    .on('error', notify.onError({ message: 'Popup.js: <%= error.message %>' }))
     .pipe(source('Popup.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
       // Add transformation tasks to the pipeline here.
       .pipe(uglify())
-      .on('error', gutil.log)
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/Popup'));
 });
@@ -62,26 +71,22 @@ gulp.task('copy', () => {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('css', () =>
+gulp.task('css', () => {
   gulp
     .src([
       '!./src/TypeIcon/*.scss',
       './src/**/*.scss',
     ])
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on('error', notify.onError({ message: '<%= error.message %>' })))
     .pipe(cleancss())
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist'))
-);
+    .pipe(gulp.dest('./dist'));
+});
 
-gulp.task('css:watch', () =>
-  gulp
-    .src('./src/**/*.scss')
-    .pipe(watch('./src/**/*.scss'))
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleancss())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist'))
-);
+gulp.task('css:watch', () => {
+  if (process.env.NODE_ENV === 'development') {
+    gulp
+      .watch('./src/**/*.scss', ['css']);
+  }
+});
