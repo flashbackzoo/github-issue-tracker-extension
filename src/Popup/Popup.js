27e57870dd7da7a86f63137b2680a5752832253c
David Craig <flashbackzoo@users.noreply.github.com>
2016-04-30 13:11:51 +1200

    Add a 'type' icon to tracked items (#6)

--- src/Popup/Popup.js
+++ src/Popup/Popup.js
@@ -1,7 +1,8 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const Backend = require('../Backend/Backend.js');
-const LoadingIndicator = require('../LoadingIndicator/LoadingIndicator.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Backend from '../Backend/Backend';
+import LoadingIndicator from '../LoadingIndicator/LoadingIndicator';
+import TypeIcon from '../TypeIcon/TypeIcon';
 
 class Popup extends React.Component {
   constructor(props) {
@@ -40,6 +41,30 @@ class Popup extends React.Component {
     this.handleOptionsLinkClick = this.handleOptionsLinkClick.bind(this);
     this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
     this.handleRemoveButtonClick = this.handleRemoveButtonClick.bind(this);
+  }
+
+  getItemTypeIcon(type) {
+    const icons = {
+      issues: 'M7 2.3c3.14 0 5.7 2.56 5.7 5.7S10.14 13.7 7 13.7 1.3 11.14 1.3 8s2.56-5.7 \
+        5.7-5.7m0-1.3C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7S10.86 1 7 1z m1 3H6v5h2V4z m0 \
+        6H6v2h2V10z',
+      pull: 'M11 11.28c0-1.73 0-6.28 \
+        0-6.28-0.03-0.78-0.34-1.47-0.94-2.06s-1.28-0.91-2.06-0.94c0 0-1.02 0-1 0V0L4 3l3 \
+        3V4h1c0.27 0.02 0.48 0.11 0.69 0.31s0.3 0.42 0.31 0.69v6.28c-0.59 0.34-1 0.98-1 1.72 0 \
+        1.11 0.89 2 2 2s2-0.89 2-2c0-0.73-0.41-1.38-1-1.72z m-1 2.92c-0.66 \
+        0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2 1.2 0.55 1.2 1.2-0.55 1.2-1.2 1.2zM4 \
+        3c0-1.11-0.89-2-2-2S0 1.89 0 3c0 0.73 0.41 1.38 1 1.72 0 1.55 0 5.56 0 6.56-0.59 0.34-1 \
+        0.98-1 1.72 0 1.11 0.89 2 2 2s2-0.89 2-2c0-0.73-0.41-1.38-1-1.72V4.72c0.59-0.34 1-0.98 \
+        1-1.72z m-0.8 10c0 0.66-0.55 1.2-1.2 1.2s-1.2-0.55-1.2-1.2 0.55-1.2 1.2-1.2 1.2 0.55 1.2 \
+        1.2z m-1.2-8.8c-0.66 0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2 1.2 0.55 1.2 1.2-0.55 1.2-1.2 \
+        1.2z',
+    };
+
+    if (icons[type] === void 0) {
+      return null;
+    }
+
+    return <TypeIcon pathd={icons[type]} />;
   }
 
   handleOptionsLinkClick() {
@@ -148,6 +173,7 @@ class Popup extends React.Component {
                   const updatedDay = updated.getDate();
                   const updatedMonth = this.monthNames[updated.getMonth()];
                   const updatedYear = updated.getFullYear();
+                  const typeIcon = this.getItemTypeIcon(item.type);
 
                   return (
                     <li className="tracked-item-list__tracked-item" key={item.id}>
@@ -155,6 +181,7 @@ class Popup extends React.Component {
                         <span className="tracked-item-list__tracked-item__repo">
                           {`${item.vendor} / ${item.project}`}
                         </span>
+                        {typeIcon}
                         <a
                           className="tracked-item-list__tracked-item__link"
                           href={item.url}
---
+++ src/TypeIcon/README.md
@@ -0,0 +1,3 @@
+# TypeIcon
+
+Icon displayed next to list items. Indicates the 'type' of item, issue, or pull request.
